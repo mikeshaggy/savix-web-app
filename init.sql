58328bd5c9d41f013671c1f1e9e73d2d70afa467
mikeshaggy <michalbagan@icloud.com>
2026-01-02 10:07:44 +0100

    sql: wallets feature

--- init.sql
+++ init.sql
@@ -5,29 +5,42 @@ create table users (
     created_at timestamp default NOW()
 );
 
+create table wallets (
+    id serial primary key,
+    user_id int not null references users(id) on delete cascade,
+    name varchar(50) not null,
+    balance numeric(12,2) not null default 0.00,
+    created_at timestamp default NOW(),
+    unique(user_id, name)
+);
+
 create table categories (
     id serial primary key,
-    fk_user_id int not null references users(id) on delete cascade,
+    user_id int not null references users(id) on delete cascade,
     name varchar(50) not null,
     type varchar(20) not null check (type in ('INCOME', 'EXPENSE')),
     created_at timestamp default NOW(),
-    unique(fk_user_id, name, type)
+    unique(user_id, name, type)
 );
 
 create table transactions (
     id bigserial primary key,
-    fk_user_id int not null references users(id) on delete cascade,
-    fk_category_id int not null references categories(id) on delete cascade,
+    wallet_id int not null references wallets(id) on delete cascade,
+    category_id int not null references categories(id) on delete cascade,
     title varchar(50) not null,
     amount numeric(12,2) not null,
     transaction_date date not null,
     notes text,
     importance varchar(20) check (importance in (
-        'ESSENTIAL', 'HAVE_TO_HAVE', 'NICE_TO_HAVE', 'SHOULDNT_HAVE'
+        'ESSENTIAL', 'HAVE_TO_HAVE', 'NICE_TO_HAVE', 'SHOULDNT_HAVE', 'INVESTMENT'
     )),
     cycle varchar(20) not null check (cycle in (
         'ONE_TIME', 'MONTHLY', 'YEARLY', 'WEEKLY', 'IRREGULAR'
     )),
-    type varchar(20) not null check (type in ('INCOME', 'EXPENSE')),
     created_at timestamp default NOW()
 );
+
+CREATE INDEX idx_transactions_wallet ON transactions(wallet_id);
+CREATE INDEX idx_transactions_category ON transactions(category_id);
+CREATE INDEX idx_transactions_date ON transactions(transaction_date);
+CREATE INDEX idx_categories_user ON categories(user_id);
